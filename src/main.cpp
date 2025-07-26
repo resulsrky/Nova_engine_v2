e74dd704505602574b5ae9e0672dfd37f6b27383
abkarada <abdurrahmankaradag19@gmail.com>
2025-07-26 10:40:19 +0300

    Engineering MasterPiece

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,22 @@
 #include <thread>
 #include <vector>
 #include <string>
+#include <iostream>
 
-std::string target_ip = "192.168.1.12";  // hedef cihazın IP'si
-std::vector<int> target_ports = {45094, 44824, 33061, 55008};
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        std::cerr << "Usage: ./novaengine <public_ip> <port1> <port2> ...\n";
+        return 1;
+    }
 
-int main() {
-    // sender thread'i başlat
+    std::string target_ip = argv[1];
+    std::vector<int> target_ports;
+    for (int i = 2; i < argc; ++i) {
+        target_ports.push_back(std::stoi(argv[i]));
+    }
+
+    // Sender thread
     std::thread sender_thread([&]() {
-        int dummy_argc = 1 + target_ports.size() + 1;
         std::vector<std::string> args = {"sender", target_ip};
         for (int port : target_ports)
             args.push_back(std::to_string(port));
@@ -21,7 +29,7 @@ int main() {
         run_sender(argv_c.size(), argv_c.data());
     });
 
-    // receiver thread'i başlat
+    // Receiver thread
     std::thread receiver_thread(run_receiver);
 
     sender_thread.join();
